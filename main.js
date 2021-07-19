ea6a539eaf2457d99c6a04b40402f0612f4fcbf9
jahnavi-2010 <80617464+jahnavi-2010@users.noreply.github.com>
2021-07-19 18:45:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-function preload()
-{
+noseX=0;
+noseY=0;
 
+function preload()
+
+{
+clown_nose=loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 function setup()
 {
@@ -23,11 +27,14 @@ if(results.length>0)
     console.log(results);
     console.log("nose x ="+results[0].pose.nose.x);
     console.log("nose y ="+results[0].pose.nose.y);
+    noseX=results[0].pose.nose.x-15;
+    noseY=results[0].pose.nose.y;
 }
 }
 function draw()
 {
 image(video,0,0,350,350);
+image(clown_nose,noseX,noseY,30,30);
 }
 function take_pic()
 {
